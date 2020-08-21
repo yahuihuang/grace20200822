df1a0fbdd57b827c0c3d9911ca4b951ad35e5a32
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-08-22 00:10:10 +0800

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { chartAreaDemo } from './chartAreaDemo';
+import { chartPieDemo } from './chartPieDemo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'grace20200822';
+
+  ngOnInit(): void {
+    // Set new default font family and font color to mimic Bootstrap's default styling
+    chartAreaDemo();
+
+    // Set new default font family and font color to mimic Bootstrap's default styling
+    chartPieDemo();
+
+  }
 }
