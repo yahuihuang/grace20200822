e37ad78872a43e3671f4057a4c0748ccf11ee9db
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-08-22 11:27:29 +0800

    加入子頁

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { ColorComponent } from './utilities/color/color.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
     { path: 'page1', component: Page1Component },
     { path: 'page2', component: Page2Component },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'utilities',
+      children: [
+        { path: 'color', component: ColorComponent }
+      ]
+    },
     { path: '**', component: PathNotFoundComponent },
   ];
 
