b4e2483afe85939f34b8f31db65087dc68d537e4
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-08-22 10:50:30 +0800

    routerLinkActiveOptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//import { chartAreaDemo } from './chartAreaDemo';
-//import { chartPieDemo } from './chartPieDemo';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +9,5 @@ export class AppComponent implements OnInit {
   title = 'grace20200822';
 
   ngOnInit(): void {
-    // Set new default font family and font color to mimic Bootstrap's default styling
-    //chartAreaDemo();
-
-    // Set new default font family and font color to mimic Bootstrap's default styling
-    //chartPieDemo();
-
   }
 }
